d7872725ed9ea7970bc2b4dab37e7dd949a98c7b
Tania Nardandrea-Isakovic <rtnrtn@gmail.com>
2020-08-12 22:30:25 +0000

    Fetching tweets with AJAX

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,31 +4,6 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const tweetData = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-];
-
 const createTweetElement = function(object) {
   let timestamp = `${object.created_at}`;
   let relativeDate = moment.unix(timestamp / 1000).fromNow();
@@ -62,18 +37,23 @@ const renderTweets = function(array) {
   }
 };
 
-// $(document).ready(function() {
-//   renderTweets(tweetData);
-// });
+const loadTweets = function() {
+  $.ajax({ url: '/tweets', method: 'GET' })
+    .then(function(response) {
+      console.log(response);
+      renderTweets(response);
+    });
+};
 
 $(document).ready(function() {
   $('#new-tweet').on('submit', function(event) {
     let tweet = $(this).serialize();
     let postURL = $(this).attr("action");
     event.preventDefault();
-    $.ajax({ url: postURL , method: 'POST', data: tweet })
-      .then(function(response) {
-        renderTweets(response);
-      });
+    $.ajax({ url: postURL , method: 'POST', data: tweet });
+      
   });
+
+  loadTweets();
+
 });
