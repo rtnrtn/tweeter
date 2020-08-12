24eab1cff12fba2bc0011c60e353a9e9e60a14ae
Tania Nardandrea-Isakovic <rtnrtn@gmail.com>
2020-08-12 20:01:18 +0000

    Dynamic tweet rendering

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,64 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+const tweetData = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+];
+
+const createTweetElement = function(object) {
+  let timestamp = `${object.created_at}`;
+  let relativeDate = moment.unix(timestamp / 1000).fromNow();
+  let $tweet = `
+    <article class="tweet">
+      <header>
+        <div class="profile">
+          <img src="${object.user.avatars}"/>
+          <h7>${object.user.name}<h7>
+        </div>
+        <div class="username">
+          <h7>${object.user.handle}</h7>
+        </div>
+      </header>
+      <p>${object.content.text}</p>
+      <footer>
+        <div>${relativeDate}</div>
+        <div class="social-actions">
+          <img src="images/flag.png">
+          <img src="images/retweet.png">
+          <img src="images/like.png">
+        </div>
+      </footer>
+  `;
+  $('#all-tweets').append($tweet);
+};
+
+const renderTweets = function(array) {
+  for (let tweet of array) {
+    createTweetElement(tweet);
+  }
+};
+
+$(document).ready(function() {
+  renderTweets(tweetData);
+});
